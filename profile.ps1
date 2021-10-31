7d9eae084dd9748d6b4c0b65de9851d64b5ed64b
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2021-10-31 08:41:48 +0200

    add goto snippet

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,5 @@
+Set-Location -Path ($MyInvocation.MyCommand.Path -replace '\\[^\\]+$')
+
 Register-ObjectEvent -InputObject ([System.IO.FileSystemWatcher] (Resolve-Path -Path .).Path) -EventName Changed -Action {
     if (($Event.SourceArgs)[1].Name -eq 'batchshell.code-snippets') {
         Copy-Item -Path 'batchshell.code-snippets' -Destination "$($Env:AppData)\Code\User\snippets\batchshell.code-snippets" -Force
